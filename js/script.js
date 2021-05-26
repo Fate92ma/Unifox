9d6c60d3dfcfe4362f5d8dba997f29069be6084d
fate92ma <fatema131khaled@gmail.com>
2021-05-26 07:56:35 +0200

    clean script enhance style

--- js/script.js
+++ js/script.js
@@ -4,16 +4,6 @@ $(document).ready(function () { // Start Coding Here
 
     $(".preloader").fadeOut(3000);
 
-    //  Caption
-
-    $(".portfolio-wrapper").mouseenter(function () {
-        $(this).children(".portfolio-caption").addClass("up");
-    });
-
-    $(".portfolio-wrapper").mouseleave(function () {
-        $(this).children(".portfolio-caption").removeClass("up");
-    });
-
     // On Scroll
 
     $(window).scroll(function () {
@@ -41,12 +31,6 @@ $(document).ready(function () { // Start Coding Here
         }, 1000)
     });
 
-    // Mini ul
-
-    $(".menu").click(function () {
-        $(".container1 ul").toggleClass("mini");
-    });
-
     // Smooth Scroll
 
     $("header .container1 ul li a").click(function () {
@@ -59,6 +43,12 @@ $(document).ready(function () { // Start Coding Here
 
     });
 
+    // Mini ul
+
+    $(".menu").click(function () {
+        $(".container1 ul").toggleClass("mini");
+    });
+
     // Count To
 
     $(window).scroll(function () {
@@ -69,6 +59,51 @@ $(document).ready(function () { // Start Coding Here
                 onUpdate: true,
             });
         };
+    });
+
+    // Portfolios
+
+    $(function () {
+        var filterList = {
+            init: function () {
+                $('#portfoliolist').mixItUp({
+                    selectors: {
+                        target: '.portfolio',
+                        filter: '.filter'
+                    },
+                    load: {
+                        filter: '.design'
+                    }
+                });
+            }
+        };
+        filterList.init();
+    });
+
+    // circle1
+
+    $('.circle1').circleProgress({
+        value: 0.90,
+        size: 150,
+        animation: {
+            duration: 20000
+        },
+        emptyFill: "white",
+        startAngle: -2, // not important
+        fill: "#049dff",
+    });
+
+    // circle2
+
+    $('.circle2').circleProgress({
+        value: 0.80,
+        size: 150,
+        animation: {
+            duration: 20000
+        },
+        emptyFill: "white",
+        startAngle: -2, // not important
+        fill: "#fdba04",
     });
 
     // Team
@@ -90,6 +125,41 @@ $(document).ready(function () { // Start Coding Here
     $("[tab]").on("click", function () {
         $('[tab]').removeClass('active');
         $(this).addClass('active');
+    });
+
+    // Owl
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        duration: 2000,
+        margin: 10,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            768: {
+                items: 2,
+            }
+        }
+    });
+
+    // Feed
+
+    var swiper = new Swiper('.swiper-container', {
+        autoHeight: true,
+        autoplay: true,
+        duration: 2000,
+        spaceBetween: 20,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
     });
 
 }); // End Code
